6272cd81a1507aac29872e7f0402f4cbc27671e5
Daniel Bright <brahyt@gmail.com>
2019-08-12 16:12:01 -0700

    changed rentals

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,5 +2,11 @@ import DS from 'ember-data';
 const { Model } = DS;
 
 export default Model.extend({
-
+  title: DS.attr(),
+  owner: DS.attr(),
+  city: DS.attr(),
+  catagoty: DS.attr(),
+  image: DS.attr(),
+  bedrooms: DS.attr(),
+  description: DS.attr()
 });
